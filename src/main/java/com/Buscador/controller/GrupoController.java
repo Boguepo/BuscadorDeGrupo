4dd7b7e0a394917b15444de0502d730d2b73af40
Boguepo <46709093+Boguepo@users.noreply.github.com>
2021-03-16 21:09:29 +0100

    Controllers Jugador, Juego y Grupo (falta validar endpoints)

---
+++ src/main/java/com/Buscador/controller/GrupoController.java
@@ -0,0 +1,67 @@
+package com.Buscador.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Buscador.dto.Grupo;
+import com.Buscador.dto.Juego;
+import com.Buscador.service.GrupoServiceImpl;
+
+@RestController
+@RequestMapping("/lfg")
+public class GrupoController {
+
+	@Autowired
+	GrupoServiceImpl grupoService;
+	
+	@GetMapping("/grupos")
+	public List<Grupo> listarGrupos(){
+		return grupoService.listarGrupos();
+	}
+	
+	@GetMapping("/grupos")
+	public List<Grupo> listarXjuego(@RequestBody Juego juego){
+		return grupoService.grupoXjuego(juego);
+	}
+	
+	@PostMapping("/grupo")
+	public Grupo guardarGrupo(@RequestBody Grupo grupo) {
+		return grupoService.guardarGrupo(grupo);
+	}
+	
+	@GetMapping("/grupo/{id}")
+	public Grupo grupoXID(@PathVariable(name = "id") Long id) {
+		return grupoService.grupoXID(id);
+	}
+	
+	@PutMapping("/grupo/{id}")
+	public Grupo actualizarGrupo(@PathVariable(name = "id") Long id, @RequestBody Grupo grupo) {
+		
+		Grupo g_s = new Grupo();
+		Grupo g_a = new Grupo();
+		
+		g_s = grupoService.grupoXID(id);
+		
+		g_s.setJuego(grupo.getJuego());
+		g_s.setNombre(grupo.getNombre());
+		g_s.setJugador(grupo.getJugador());
+		
+		g_a = grupoService.actualizarGrupo(g_s);
+		
+		return g_a;
+	}
+	
+	@DeleteMapping("/grupo/{id}")
+	public void borrarGrupo(@PathVariable(name = "id") Long id) {
+		grupoService.borrarGrupo(id);
+	}
+}
---
+++ src/main/java/com/Buscador/controller/JuegoController.java
@@ -0,0 +1,61 @@
+package com.Buscador.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Buscador.dto.Juego;
+import com.Buscador.service.JuegoServiceImpl;
+
+@RestController
+@RequestMapping("/lfg")
+public class JuegoController {
+
+	@Autowired
+	JuegoServiceImpl juegoService;
+	
+	@GetMapping("/juegos")
+	public List<Juego> listarJuegos(){
+		return juegoService.listarJuegos();
+	}
+	
+	@PostMapping("/juego")
+	public Juego guardarJuego(@RequestBody Juego juego) {
+		return juegoService.guardarJuego(juego);
+	}
+	
+	@GetMapping("/juego/{id}")
+	public Juego juegoXID(@PathVariable(name = "id") Long id) {
+		return juegoService.juegoXID(id);
+	}
+	
+	@PutMapping("/juego/{id}")
+	public Juego actualizarJuego(@PathVariable(name = "id") Long id, @RequestBody Juego juego) {
+		
+		Juego ju_s = new Juego();
+		Juego ju_a = new Juego();
+		
+		ju_s = juegoService.juegoXID(id);
+		
+		ju_s.setTitulo(juego.getTitulo());
+		ju_s.setThumbnail(juego.getThumbnail());
+		ju_s.setUrl(juego.getUrl());
+		
+		ju_a = juegoService.actualizarJuego(ju_s);
+		
+		return ju_a;
+	}
+	
+	@DeleteMapping("/juego/{id}")
+	public void borrarJuego(@PathVariable(name = "id") Long id) {
+		juegoService.borrarJuego(id);
+	}
+}
