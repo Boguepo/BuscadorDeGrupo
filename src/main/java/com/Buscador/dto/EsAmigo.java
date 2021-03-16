ba3a9db67c4051f9b0b4b881633d9092eb40eba3
Sergio <sergio_castilla_13@hotmail.com>
2021-03-16 21:04:41 +0100

    DTO de EsAmigo y JugadorGrupo

--- src/main/java/com/Buscador/dto/EsAmigo.java
+++ src/main/java/com/Buscador/dto/EsAmigo.java
@@ -1,5 +1,85 @@
 package com.Buscador.dto;
 
-public class EsAmigo {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="Es_amigo")
+public class EsAmigo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//busca ultimo valor e incrementa desde id final de db
+	@Column(name="id_a")
+	private long id;
+	
+	@ManyToOne
+    @JoinColumn(name = "id_user")
+    Jugador user;
+ 
+    @ManyToOne
+    @JoinColumn(name = "id_amigo")
+    Jugador amigo;
+	
+    @Temporal(TemporalType.TIMESTAMP)
+	private Date aceptado;
+
+	public EsAmigo() {
+		super();
+	}
+
+	public EsAmigo(long id, Jugador user, Jugador amigo, Date aceptado) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.amigo = amigo;
+		this.aceptado = aceptado;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Jugador getUser() {
+		return user;
+	}
+
+	public void setUser(Jugador user) {
+		this.user = user;
+	}
+
+	public Jugador getAmigo() {
+		return amigo;
+	}
+
+	public void setAmigo(Jugador amigo) {
+		this.amigo = amigo;
+	}
+
+	public Date getFecha() {
+		return aceptado;
+	}
+
+	public void setFecha(Date aceptado) {
+		this.aceptado = aceptado;
+	}
+
+	@Override
+	public String toString() {
+		return "EsAmigo [id=" + id + ", user=" + user + ", amigo=" + amigo + ", aceptado=" + aceptado + "]";
+	}
+	
+	
 }
--- src/main/java/com/Buscador/dto/JugadorGrupo.java
+++ src/main/java/com/Buscador/dto/JugadorGrupo.java
@@ -1,5 +1,85 @@
 package com.Buscador.dto;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="Jugador_grupo")
 public class JugadorGrupo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//busca ultimo valor e incrementa desde id final de db
+	@Column(name="id_jg")
+	private long id;
+	
+	@ManyToOne
+    @JoinColumn(name = "id_user")
+    Jugador jugador;
+ 
+    @ManyToOne
+    @JoinColumn(name = "id_grupo")
+    Grupo grupo;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date fecha;
+
+	public JugadorGrupo() {
+		super();
+	}
+
+	public JugadorGrupo(long id, Jugador jugador, Grupo grupo, Date fecha) {
+		super();
+		this.id = id;
+		this.jugador = jugador;
+		this.grupo = grupo;
+		this.fecha = fecha;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Jugador getJugador() {
+		return jugador;
+	}
+
+	public void setJugador(Jugador jugador) {
+		this.jugador = jugador;
+	}
+
+	public Grupo getGrupo() {
+		return grupo;
+	}
+
+	public void setGrupo(Grupo grupo) {
+		this.grupo = grupo;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	@Override
+	public String toString() {
+		return "JugadorGrupo [id=" + id + ", jugador=" + jugador + ", grupo=" + grupo + ", fecha=" + fecha + "]";
+	}
+	
 
 }
